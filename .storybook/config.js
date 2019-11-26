2868a530637de2ab0e23a624a6c264a527516137
sugoikondo <atsu9615@me.com>
2019-11-26 23:21:44 +0900

    locale指定して動くようになったぁぁぁあｌ

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/vue';
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
 
-Vue.use(VueI18n);
+import { addDecorator } from '@storybook/vue';
+import i18n from '@/plugins/i18n';
+
+addDecorator(() => ({
+  template: '<story/>',
+  i18n,
+}));
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
--- stories/components/parts/flowSns.stories.js
+++ stories/components/parts/flowSns.stories.js
@@ -1,11 +1,11 @@
 import { storiesOf } from '@storybook/vue'
 
 import FlowSns from '@/components/parts/flowSns'
+import { i18n } from '../../plugins/i18n'
 
 storiesOf('FlowSns', module)
   .add('default', () => ({
     components: { FlowSns },
-    render(h) {
-      return <FlowSns />
-    }
+    template: `<FlowSns />`,
+    i18n
   }))
